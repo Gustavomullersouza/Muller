bc5f8d3dee7828af468f47803fe9fc0f2e4ada36
gustavomullersouza <gustavomullerdesouza@gmail.com>
2024-05-10 08:17:09 -0300

    arquivo scrip add

---
+++ Galeria-imagens/scripts-basico.js
@@ -0,0 +1,24 @@
+//O vento DOM é acionado quando o HTML foi completamente carregado
+
+document.addEventListener('DOMContentLoaded', function () {
+
+    alert(123);
+
+    //Tipos de dados
+    //String: Sequencia de caracter
+    //Number: Números inteiros ou pontos flutuantes (10.4)
+    //booLeon: Rtona dois valores, true ou false
+    //object: estrutura de dados que pode armazenar multiplos valores
+    //NULL: quanto está vazio;
+    //array: objeto especial ordenado
+    //function: bloco de codigo reutilizável
+
+    //É usado para declarar uma variavel cujo valor é permanente
+    const test = 50;
+
+    //é usado para declarar variáveis que nao podem ser
+    //retribuidos
+    let C = 0;
+    C = 10;
+    console.log(C)
+})
