e8092e08ca53061b011da9d6c397e2b263fb30e1
gustavomullersouza <gustavomullerdesouza@gmail.com>
2024-05-17 09:34:08 -0300

    scripts js

---
+++ Galeria-imagens/scripts.js
@@ -0,0 +1,34 @@
+
+document.addEventListener('DOMContentLoaded', function () {
+
+        const baseImagens = {
+            todas: ['imagem.png', 'cidade.jpg', 'animal.jpg', 'natureza.jpg'],
+            natureza: [],
+            cidade: [],
+            animais: [],
+            
+        }
+
+    // Evento de clique
+    document.body.addEventListener('click', function(event) {
+            // acessa a lista de classe de onde clicou  e
+            // procura pela classe botao-categoria
+        if(event.target.classList.contains('botao-categoria')) {   
+        
+            
+        }
+    
+    })
+
+
+
+
+
+
+
+
+
+
+
+
+} ) 
